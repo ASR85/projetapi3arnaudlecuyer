6578a3486ebdca6af5d407116bf3d75511fd9325
Arnaud LECUYER <arnaud.lecuyer@condorcet.be>
2022-10-17 14:19:46 +0200

    Initial commit

---
+++ src/main/java/com/example/projetapi3arnaudlecuyer/Projetapi3arnaudlecuyerApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetapi3arnaudlecuyer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projetapi3arnaudlecuyerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Projetapi3arnaudlecuyerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projetapi3arnaudlecuyer/Projetapi3arnaudlecuyerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetapi3arnaudlecuyer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projetapi3arnaudlecuyerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
